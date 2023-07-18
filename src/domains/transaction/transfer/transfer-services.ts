e78c91cc94c0698fb3b5091da8dd8dbd31ec6f46
kevinirawand <kevinid07@gmail.com>
2023-07-18 14:38:54 +0700

    Transfer | Transaction.Transfer

---
+++ src/domains/transaction/transfer/transfer-services.ts
@@ -0,0 +1,5 @@
+class TransferServices {
+   public send = async (): Promise<void> => {};
+}
+
+export default new TransferServices();
---
+++ src/models/transaction.ts
@@ -0,0 +1,35 @@
+'use strict';
+import { Model } from 'sequelize';
+module.exports = (sequelize: any, DataTypes: any) => {
+   class Transaction extends Model {
+      /**
+       * Helper method for defining associations.
+       * This method is not a part of Sequelize lifecycle.
+       * The `models/index` file will call this method automatically.
+       */
+      static associate(models: any) {
+         // define association here
+      }
+   }
+   Transaction.init(
+      {
+         id: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            primaryKey: true,
+            defaultValue: DataTypes.UUIDV4,
+         },
+         wallet_sender: DataTypes.STRING,
+         wallet_receiver: DataTypes.STRING,
+         phone_number_receiver: DataTypes.STRING,
+         amount: DataTypes.FLOAT,
+         status: DataTypes.ENUM('success', 'fail'),
+      },
+      {
+         sequelize,
+         modelName: 'Transaction',
+         underscored: true,
+      },
+   );
+   return Transaction;
+};
