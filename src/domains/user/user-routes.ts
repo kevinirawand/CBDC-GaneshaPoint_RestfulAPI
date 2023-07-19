42b0f7e96542c345da9eecf54ba1b69674839500
kevinirawand <kevinid07@gmail.com>
2023-07-19 13:47:39 +0700

    Test Auth Token Middleware | User

--- src/domains/user/user-routes.ts
+++ src/domains/user/user-routes.ts
@@ -1,14 +1,24 @@
 import UserController from './user-controller';
 import BaseRoutes from '../../base_claseses/base-routes';
 import tryCatch from '../../utils/tryCatcher';
+import authToken from '../../middlewares/auth-token-middleware';
 
 class UserRoutes extends BaseRoutes {
    public routes(): void {
-      this.router.get('/', tryCatch(UserController.index));
-      this.router.post('/create', tryCatch(UserController.create));
-      this.router.get('/show/:user_id', tryCatch(UserController.show));
-      this.router.put('/update/:user_id', tryCatch(UserController.update));
-      this.router.delete('/delete/:user_id', tryCatch(UserController.delete));
+      this.router.get('/', [authToken, tryCatch(UserController.index)]);
+      this.router.post('/create', [authToken, tryCatch(UserController.create)]);
+      this.router.get('/show/:user_id', [
+         authToken,
+         tryCatch(UserController.show),
+      ]);
+      this.router.put('/update/:user_id', [
+         authToken,
+         tryCatch(UserController.update),
+      ]);
+      this.router.delete('/delete/:user_id', [
+         authToken,
+         tryCatch(UserController.delete),
+      ]);
    }
 }
 
