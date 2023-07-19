dade600416d0701bd42a56d2218a292c6cbaad89
kevinirawand <kevinid07@gmail.com>
2023-07-19 13:46:01 +0700

    Add Auth Token Middleware | Auth

---
+++ src/middlewares/auth-token-middleware.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from 'express';
+import statusCodes from '../errors/status-codes';
+import BaseError from '../base_claseses/base-error';
+import jwt from 'jsonwebtoken';
+
+const authToken = (req: Request, res: Response, next: NextFunction): any => {
+   const authHeader: string | undefined = req.get('X-Auth');
+
+   const token: string | undefined = authHeader && authHeader.split(' ')[1];
+
+   if (token == null)
+      throw new BaseError(
+         401,
+         statusCodes.UNAUTHORIZE.message,
+         'User Have Not Login',
+      );
+
+   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY || '', (err, user) => {
+      if (err) {
+         if (err.message == 'invalid signature') {
+            throw new BaseError(
+               403,
+               statusCodes.FORBIDDEN.message,
+               'Invalid Signature',
+            );
+         } else {
+            throw new BaseError(
+               403,
+               statusCodes.FORBIDDEN.message,
+               'Token Is Invalid Or No Longer Valid',
+            );
+         }
+      }
+
+      req.app.locals.user = user;
+
+      return next();
+   });
+};
+
+export default authToken;
