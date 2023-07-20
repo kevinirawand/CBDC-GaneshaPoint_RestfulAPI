cdf3a77ee6d1d0bc464cebf212931ecbc0eb5211
kevinirawand <kevinid07@gmail.com>
2023-07-20 11:52:58 +0700

    Add hasOne to User

--- src/models/wallet.ts
+++ src/models/wallet.ts
@@ -7,7 +7,9 @@ module.exports = (sequelize: any, DataTypes: any) => {
        * This method is not a part of Sequelize lifecycle.
        * The `models/index` file will call this method automatically.
        */
-      static associate(models: any) {}
+      static associate(models: any) {
+         Wallet.hasOne(models.User);
+      }
    }
 
    Wallet.init(
