5e1a08d7b8c1c82e84c23dda6e531ce8c98f2f65
kevinirawand <kevinid07@gmail.com>
2023-07-24 12:12:02 +0700

    Change locale Datetime

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,25 +1,41 @@
 require('dotenv').config()
 
 module.exports = {
-   "development": {
-      "username": process.env.DB_USERNAME,
-      "password": process.env.DB_PASSWORD,
-      "database": process.env.DB_NAME,
-      "host": process.env.DB_HOST,
-      "dialect": process.env.DIALECT
+   development: {
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      dialect: process.env.DIALECT,
+      charset: "utf8mb4",
+      timezone: "+07.00",
+      logger: null,
+      timezone: "Asia/Jakarta",
+      dialectOptions: {
+         useUTC: false,
+         collate: "utf8mb4_general_ci",
+      }
    },
-   "test": {
-      "username": "root",
-      "password": null,
-      "database": "database_test",
-      "host": "127.0.0.1",
-      "dialect": "mysql"
+   test: {
+      username: 'root',
+      password: null,
+      database: 'database_test',
+      host: '127.0.0.1',
+      dialect: 'mysql'
    },
-   "production": {
-      "username": process.env.DB_USERNAME,
-      "password": process.env.DB_PASSWORD,
-      "database": process.env.DB_NAME,
-      "host": process.env.DB_HOST,
-      "dialect": process.env.DIALECT
+   production: {
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      dialect: process.env.DIALECT,
+      charset: "utf8mb4",
+      timezone: "+07.00",
+      logger: null,
+      timezone: "Asia/Jakarta",
+      dialectOptions: {
+         useUTC: false,
+         collate: "utf8mb4_general_ci",
+      }
    }
 }
